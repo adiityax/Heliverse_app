43111638ab0ea59050e69c62b8d4834b44f86b26
adiityax <64186221+adiityax@users.noreply.github.com>
2022-06-27 21:23:12 +0530

    Adding all files

---
+++ my_app/src/App.js
@@ -0,0 +1,13 @@
+import Signup from './components/Signup';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Signup />
+    </div>
+  );
+}
+
+export default App;
---
+++ my_app/src/components/Success.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ResponsiveAppBar from './Appbar.tsx'
+
+const Success = () => {
+  return (
+    <div>
+      <ResponsiveAppBar />
+      <h1>You are done!</h1>
+    </div>
+  )
+}
+
+export default Success
